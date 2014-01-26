437ef7e227b71f635c19a5f242f5e21242a86133
Ubuntu <ubuntu@ip-172-31-18-89.us-west-2.compute.internal>
2014-01-26 10:34:13 +0000

    separating time and date

--- src/add-hitch.php
+++ src/add-hitch.php
@@ -1,21 +1,38 @@
 <?php
-
 	require_once 'lib/firebaseLib.php';
+	
+	//$place = "HOU";
+	//$datetime = "01-20-3201";
+	$place = $_GET["place"];
+	$date = $_GET["date"];
+	$time = $_GET["time"];
+
+	//$user = $_COOKIE["user"];
+	$user = "amn2";
+
+	/* get name from netid*/
+	$name = "name".$user;
 
 	// --- set up your own database here
 	$url = 'https://ymn.firebaseio.com/';
 	$token = null;
 
-	$path = "/hackrice/10%2F25%2F2013";
+	$path = "/hackrice/" . $date . "/" . $place;
+	//echo($path . "\n");
 
 	$fb = new fireBase($url, $token);
 
 	$hitch = array(
-	  'name' => 'buh',
+	  $user => array(
+		"name" => $name,
+		"time" => $time
+	  )
 	);
 
 	$response = $fb->set($path, $hitch);
 
-	echo($response);
+	//echo($response);
+
+	header( 'Location: results.php?date=' . $date . '&place=' . $place ) ;
 
 ?>
